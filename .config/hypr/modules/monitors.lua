d008585d7f431feaa5f7f0e64bf8afa787145131
Patrick Monteiro <patrickovm.eng@gmail.com>
2026-07-10 17:28:27 -0300

    refactor: migrate hyprland config to lua

---
+++ .config/hypr/modules/monitors.lua
@@ -0,0 +1,17 @@
+------------------
+---- MONITORS ----
+------------------
+
+-- See https://wiki.hypr.land/Configuring/Basics/Monitors/
+hl.monitor({
+	output = "DP-3",
+	mode = "2560x1440@180",
+	position = "0x0",
+	scale = "1",
+})
+
+hl.monitor({
+	output = "HDMI-A-1",
+	mode = "1920x1080@74.97",
+	disabled = true,
+})
